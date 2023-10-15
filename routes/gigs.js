483d6fa992a24f87f5ddef519eed7be1d20c8c57
anix001 <chaudharyanish305@gmail.com>
2023-10-15 17:50:20 +0545

    Feature: Add Gig Route

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+//Get All Gigs
 router.get('/', async(req, res)=> 
    await Gig.findAll()
     .then(gigs=>{
@@ -11,5 +12,21 @@ router.get('/', async(req, res)=>
     })
     .catch(err=> console.log(err))
 );
+
+//Add a Gig to db
+router.post('/add', (req, res)=>{
+
+    let {title, technologies, budget, description, contact_email} = req.body;
+
+    //Insert into table
+    Gig.create({
+        title,
+        technologies,
+        budget,
+        description,
+        contact_email
+    }).then(gigs=> res.redirect('/gigs'))
+    .catch(err=> console.log("Error: ", err));
+})
 
 module.exports = router;
